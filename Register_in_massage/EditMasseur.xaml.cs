6da5e5a873b37e0b3949fa0129205d3bf4a79fb2
Даниил <dragon0lordchik@gmail.com>
2022-02-28 09:57:32 +0300

    Добавил редактирование массажистов и юзеров

---
+++ Register_in_massage/EditMasseur.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Register_in_massage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditMasseur : ContentPage
+    {
+        Masseur masseur;
+        public EditMasseur(Masseur newMass)
+        {
+            InitializeComponent();
+            masseur = newMass;
+
+            txt_EditMsName.Text = masseur.Name;
+            txt_EditMsSecName.Text = masseur.SecName;
+            txt_EditMsWork.Text = masseur.WorkExperience.ToString();
+            txt_EditMsNum.Text = masseur.Number;
+        }
+
+        private void Cancel(object sender, EventArgs e)
+        {
+            this.Navigation.PopAsync();
+        }
+
+        private async void ButtonSave_Clicked(object sender, EventArgs e)
+        {
+            Masseur mas = new Masseur
+            {
+                Id = masseur.Id,
+                Name = txt_EditMsName.Text,
+                SecName = txt_EditMsSecName.Text,
+                WorkExperience = int.Parse(txt_EditMsWork.Text),
+                Number = txt_EditMsNum.Text
+            };
+
+            bool result = await DisplayAlert("Подтвердить действие", $"Вы точно хотите изменить \n{mas.Name}?", "Да", "Нет");
+
+            if (result == true)
+            {
+                try
+                {
+                    if (!String.IsNullOrEmpty(mas.Name))
+                    {
+                        App.Database.SaveMasseur(mas);
+                    }
+                    //ProjectsPage projectsPage = new ProjectsPage();
+                    //await Navigation.PushAsync(projectsPage);
+                }
+                catch (Exception)
+                {
+                    _ = DisplayAlert("Подтвердить действие", "Укажите имя", "Ок");
+                }
+            }
+        }
+
+        private async void Delete_Clicked(object sender, EventArgs e)
+        {
+            bool result = await DisplayAlert("Подтвердить действие", $"Вы точно хотите удалить \n{masseur.Name}?", "Да", "Нет");
+
+            if (result == true)
+            {
+                App.database.DeleteUser(masseur.Id);
+                //ProjectsPage projectsPage = new ProjectsPage();
+                //await Navigation.PushAsync(projectsPage);
+            }
+        }
+    }
+}
---
+++ Register_in_massage/EditUser.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Register_in_massage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditUser : ContentPage
+    {
+        User user;
+        public EditUser(User newUsr)
+        {
+            InitializeComponent();
+            user = newUsr;
+
+            txt_EditUsName.Text = user.Name;
+            txt_EditUsSecName.Text = user.SecName;
+            txt_EditUsYear.Text = user.Year;
+            txt_EditUsPas.Text = user.Password;
+            txt_EdirUsEmail.Text = user.Email;
+            txt_EditUsNum.Text = user.Number;
+        }
+
+        private void Cancel(object sender, EventArgs e)
+        {
+            this.Navigation.PopAsync();
+        }
+
+        private async void ButtonSave_Clicked(object sender, EventArgs e)
+        {
+            User usr = new User
+            {
+                Id = user.Id,
+                Password = txt_EditUsNum.Text,
+                Year = txt_EditUsYear.Text,
+                Name = txt_EditUsName.Text,
+                SecName = txt_EditUsSecName.Text,
+                Number = txt_EditUsNum.Text,
+                Email = txt_EdirUsEmail.Text
+            };
+
+            bool result = await DisplayAlert("Подтвердить действие", $"Вы точно хотите изменить \n{user.Name}?", "Да", "Нет");
+
+            if (result == true)
+            {
+                try
+                {
+                    if (!String.IsNullOrEmpty(usr.Name))
+                    {
+                        App.Database.SaveUser(usr);
+                    }
+                    //ProjectsPage projectsPage = new ProjectsPage();
+                    //await Navigation.PushAsync(projectsPage);
+                }
+                catch (Exception)
+                {
+                    _ = DisplayAlert("Подтвердить действие", "Укажите имя", "Ок");
+                }
+            }
+        }
+
+        private async void Delete_Clicked(object sender, EventArgs e)
+        {
+            bool result = await DisplayAlert("Подтвердить действие", $"Вы точно хотите удалить \n{user.Name}?", "Да", "Нет");
+
+            if (result == true)
+            {
+                App.database.DeleteUser(user.Id);
+                //ProjectsPage projectsPage = new ProjectsPage();
+                //await Navigation.PushAsync(projectsPage);
+            }
+        }
+    }
+}
